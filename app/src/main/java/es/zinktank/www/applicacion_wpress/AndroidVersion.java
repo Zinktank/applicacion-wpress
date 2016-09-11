55b2211b1ebc7fa531638c3cb7904ffef1c0c765
Pablo Gómez <Pablo Gmez>
2016-09-11 14:19:03 +0100

    First steeps of the sample json parser

---
+++ app/src/main/java/es/zinktank/www/applicacion_wpress/AndroidVersion.java
@@ -0,0 +1,23 @@
+package es.zinktank.www.applicacion_wpress;
+
+/**
+ * Created by pablo on 11/09/2016.
+ */
+
+public class AndroidVersion {
+    private String ver;
+    private String name;
+    private String api;
+
+    public String getVer() {
+        return ver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getApi() {
+        return api;
+    }
+}
---
+++ app/src/main/java/es/zinktank/www/applicacion_wpress/JSONResponse.java
@@ -0,0 +1,12 @@
+package es.zinktank.www.applicacion_wpress;
+
+/**
+ * Created by pablo on 11/09/2016.
+ */
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
